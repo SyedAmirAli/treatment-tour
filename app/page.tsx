206f9b0a0a3fd7af0f6f005b75bc9a23fbf6957b
SyedAmirAli <sywmohammadamir@gmail.com>
2024-09-21 16:35:16 +0600

    igonre header external links

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Head from 'next/head';
-
 export default function Home() {
-    return (
-        <Head>
-            <script
-                src='https://kit.fontawesome.com/11e3e6af90.js'
-                crossOrigin='anonymous'></script>
-        </Head>
-    );
+    return <div className='text-9xl'>Home</div>;
 }
